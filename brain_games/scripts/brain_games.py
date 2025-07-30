a59cbfb0b9ee3d813646e8cfe90dcd71dfdf00c5
grimmlok <Shinigami_626@mail.ru>
2025-07-30 18:00:59 +0300

    внес изменения в фвйл brain_games.py: добавил аргументы в вызов функции

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -29,4 +29,4 @@ def run_game(generate_question, check_answer=lambda user_ans, correct_ans: user_
     print(f'Congratulations, {name}!')
 
 def main():
-    run_game()
+    run_game(generate_question, check_answer=lambda user_ans, correct_ans: user_ans.strip().lower() == str(correct_ans).strip().lower(), game_description='')

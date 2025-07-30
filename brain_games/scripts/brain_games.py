e8b296ccc47057d99317f3e806d367842af3f817
grimmlok <Shinigami_626@mail.ru>
2025-07-30 17:54:27 +0300

    добавил в brain_games.py функцию main()

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -27,3 +27,6 @@ def run_game(generate_question, check_answer=lambda user_ans, correct_ans: user_
         else:
             print('Correct!')
     print(f'Congratulations, {name}!')
+
+def main():
+    run_game()
